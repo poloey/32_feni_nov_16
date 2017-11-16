309fa4dc4a4ba4800de28717a1885a0d424168ab
Polo Dev <polodev10@gmail.com>
2017-11-16 14:14:48 +0600

    added class 32

---
+++ nizam-autoload/index.php
@@ -0,0 +1,36 @@
+<?php
+require 'vendor/autoload.php';
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'nizam',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+Capsule::schema()->dropIfExists('users');
+Capsule::schema()->create('users', function ($table) {
+    $table->increments('id');
+    $table->string('name');
+    $table->string('email')->unique();
+    $table->timestamps();
+});
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# [:house: Feni Batch Home Page](http://poloey.github.io/feni)
+
+# Class 32 
+# isotope
+# autoloading in php
